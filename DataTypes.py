3317622683c67d8ea327f22fcd0c91485653cafe
Mohammad Shojaei <shojaeireal@gmail.com>
2023-03-10 20:05:30 +0330

    Create DataTypes.py
    In this example, we define four variables of different data types, and then use the type() function to identify the data type of each variable.

---
+++ DataTypes.py
@@ -0,0 +1,24 @@
+# String Data Type
+name = "Tanjiro"
+print(name)
+print(type(name))
+
+#Integer Data Type
+age = 15
+print(age)
+print(type(age))
+
+#Float Data Type
+height = 1.65 
+print(height)
+print(type(height))
+
+#Bollean Data Type
+is_strong = True
+print(is_strong)
+print(type(is_strong))
+
+#Complex Data Type
+complex_num = 3 + 4j
+print(complex_num)
+print(type(complex_num))
