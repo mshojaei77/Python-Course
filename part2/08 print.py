260d8a6e11bfee89465be669d54aec90ad1e7284
Mohammad Shojaei <shojaeireal@gmail.com>
2023-03-10 23:51:11 +0330

    Update 08 print.py

--- part2/08 print.py
+++ part2/08 print.py
@@ -1,1 +1,22 @@
+# Basic Usage
+print("Hello, world!")
 
+# Printing Variables
+name = "John"
+age = 30
+print("My name is", name, "and I am", age, "years old.")
+print("My name is {} and I am {} years old.".format(name, age))
+print(f"My name is {name} and I am {age} years old.")
+
+# Printing to a File
+with open("output.txt", "w") as f:
+    print("Hello, file!", file=f)
+
+# Specifying Separator and End
+print("One", "Two", "Three", sep="-", end="!")
+
+# Flushing Buffer
+import time
+for i in range(5):
+    print(i, end="-", flush=True)
+    time.sleep(1)
