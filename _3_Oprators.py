a7203e74a207e5adee795f7b50dbb17c57475720
Mohammad Shojaei <shojaeireal@gmail.com>
2023-03-10 20:14:34 +0330

    Create _3_Oprators.py

---
+++ _3_Oprators.py
@@ -0,0 +1,28 @@
+# Arithmetic operators
+x = 10
+y = 20
+print(x + y)  # Output: 30
+print(x - y)  # Output: -10
+print(x * y)  # Output: 200
+print(x / y)  # Output: 0.5
+print(x % y)  # Output: 10
+print(x ** y) # Output: 100000000000000000000
+
+# Comparison operators
+print(x == y) # Output: False
+print(x != y) # Output: True
+print(x > y)  # Output: False
+print(x < y)  # Output: True
+print(x >= y) # Output: False
+print(x <= y) # Output: True
+
+# Logical operators
+a = True
+b = False
+print(a and b) # Output: False
+print(a or b)  # Output: True
+print(not a)   # Output: False
+
+# Operator precedence
+print(2 + 3 * 5)  # Output: 17
+print((2 + 3) * 5) # Output: 25
