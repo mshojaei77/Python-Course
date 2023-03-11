e46977f5bd0813c3915ea6e11943dd9f1e932fee
Mohammad Shojaei <shojaeireal@gmail.com>
2023-03-11 13:16:00 +0330

    Create q7_String add mul.py

---
+++ part2/challenges/q7_String add mul.py
@@ -0,0 +1,10 @@
+#همانند سازی و الحاق در رشته ها
+first_name = "ali"
+middle_name= "reza"
+last_name= " hamidi"
+full_name= first_name + middle_name + last_name
+print (full_name)
+
+yell = "Hi "
+repeat = yell * 3
+print (repeat)
