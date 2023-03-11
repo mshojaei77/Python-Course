e683b2aae81d173d62d421ba42a2aeac5d92a49f
Mohammad Shojaei <shojaeireal@gmail.com>
2023-03-11 11:54:11 +0330

    Update 02 data types.py

--- part2/02 data types.py
+++ part2/02 data types.py
@@ -1,24 +1,24 @@
-# String Data Type
+# String Data Type رشته 
 name = "Tanjiro"
 print(name)
 print(type(name))
 
-#Integer Data Type 
+#Integer Data Type عدد صحیح
 age = 15
 print(age)
 print(type(age))
 
-#Float Data Type
+#Float Data Type عدد اعشاری
 height = 1.65 
 print(height)
 print(type(height))
 
-#Bollean Data Type
-is_strong = True
-print(is_strong)
-print(type(is_strong))
-
-#Complex Data Type
+#Complex Data Type عدد مختلط
 complex_num = 3 + 4j
 print(complex_num)
 print(type(complex_num))
+
+#Boolean Data Type نوع داده بولی
+is_strong = True
+print(is_strong)
+print(type(is_strong))
