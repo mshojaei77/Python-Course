4e305d8296910bb4d8807754327eb3a67059cc48
Mohammad Shojaei <shojaeireal@gmail.com>
2023-03-14 18:00:56 +0330

    Update and rename 13 conditional statements.py to 13 if-else-elif.py

---
+++ examples/13 if-else-elif.py
@@ -0,0 +1,16 @@
+# Using if-else statement
+x = 5
+if x > 0:
+    print("x is positive")
+else:
+    print("x is non-positive")
+
+# Using if-elif-else statement
+y = -2
+if y > 0:
+    print("y is positive")
+elif y == 0:
+    print("y is zero")
+else:
+    print("y is negative")
+    
