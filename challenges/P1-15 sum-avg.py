305aac4e967ef761808479fa30464a7ba0f800c8
Mohammad Shojaei <shojaeireal@gmail.com>
2023-03-18 05:09:39 +0330

    Create P1-15 sum-avg.py

---
+++ challenges/P1-15 sum-avg.py
@@ -0,0 +1,9 @@
+a = int(input("a: "))
+b = int(input("a: "))
+c = int(input("a: "))
+d = int(input("a: "))
+
+sum = a + b + c + d
+avg = sum/4
+print("Summation: " , sum )
+print("Average: " , avg )
