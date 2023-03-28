3f133c2cb52d834efd06dd8fdb4dedaa669d7228
Mohammad Shojaei <shojaeireal@gmail.com>
2023-03-28 12:10:58 +0330

    Update 06 Enistein.py

--- challenges/1 Basic Concepts/06 Enistein.py
+++ challenges/1 Basic Concepts/06 Enistein.py
@@ -1,8 +1,7 @@
-# Mass–energy equivalence
+# What is the Python code to calculate the energy (E) in joules for an object of mass (m) in kilograms 
+# using Einstein's famous equation E = m * c^2, where c is the speed of light in meters per second and set as a constant of 300,000,000 m/s ?
+
 m = int(input("m: "))
-
 c = 300000000
-
 E = m * (c ** 2)
-
 print(E)
