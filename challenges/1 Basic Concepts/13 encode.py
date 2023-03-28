38220e16dad4f632b47c615963fb97b63c4946a9
Mohammad Shojaei <shojaeireal@gmail.com>
2023-03-28 12:50:25 +0330

    Update 13 encode.py

--- challenges/1 Basic Concepts/13 encode.py
+++ challenges/1 Basic Concepts/13 encode.py
@@ -1,4 +1,7 @@
-#Write a program that takes a string as input from the user and encodes it using UTF-8 encoding.
+# Write a program to encode a user-generated string into binary using UTF-16 encoding, then decode it back into Unicode.
 user_input = input("Enter a string: ")
-encoded_string = user_input.encode('utf-8')
+encoded_string = user_input.encode('utf-16')
 print("Encoded string:", encoded_string)
+
+decoded_string = encoded_string.decode('utf-16')
+print("Decoded string:", decoded_string)
