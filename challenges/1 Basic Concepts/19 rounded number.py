812ad22e3bb58457d88a557a86c985f91bc8cc95
Mohammad Shojaei <shojaeireal@gmail.com>
2023-03-28 13:09:25 +0330

    Update 19 rounded number.py

--- challenges/1 Basic Concepts/19 rounded number.py
+++ challenges/1 Basic Concepts/19 rounded number.py
@@ -1,2 +1,4 @@
-z = float(input("enter a number: "))
-print(f"{ z :.3}")
+# Write a Python program that prompts the user to enter a number and returns the number with 3 decimal places.\
+
+num = float(input("Enter a number: ")) 
+print(f"{num:.3f}") 
