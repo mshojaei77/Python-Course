e37a984cd4aef0cfc1b3bb2a3f7d2451e3cf8e96
Mohammad Shojaei <shojaeireal@gmail.com>
2023-03-28 13:07:36 +0330

    Update 18 complex .py

--- challenges/1 Basic Concepts/18 complex .py
+++ challenges/1 Basic Concepts/18 complex .py
@@ -1,3 +1,4 @@
+# Write a Python function that takes two integer parameters and returns a complex number consisting of those parameters as the real and imaginary parts, respectively.
 real = int(input("enter the real part: "))
 img = int(input("enter the imagin part: "))
 print(complex(real,img))
