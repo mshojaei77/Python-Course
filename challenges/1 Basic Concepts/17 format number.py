3b0501de1105f95d72c8ba20ec5c6dd936c5b007
Mohammad Shojaei <shojaeireal@gmail.com>
2023-03-28 13:06:44 +0330

    Update 17 format number.py

--- challenges/1 Basic Concepts/17 format number.py
+++ challenges/1 Basic Concepts/17 format number.py
@@ -1,2 +1,3 @@
+# Write a Python program that takes an input from the user, and prints out the number formatted with commas for thousands separator.
 z = int(input("enter a number: "))
 print(f"{ z :,}")
