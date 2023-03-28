1f65aee21063821181ddc90bf2360f77219e5094
Mohammad Shojaei <shojaeireal@gmail.com>
2023-03-28 12:25:21 +0330

    Update 12 minus.py

--- challenges/1 Basic Concepts/12 minus.py
+++ challenges/1 Basic Concepts/12 minus.py
@@ -1,3 +1,4 @@
+# Write a program that prompts the user to input two numbers and then calculates the absolute difference between them. 
 num1 = float(input("first number: "))
 num2 = float(input("seconde number: "))
 res = num1 - num2 
