c49a86a4c732f30ded2edbba9c7e59e0a040b98a
Mohammad Shojaei <shojaeireal@gmail.com>
2023-03-28 12:19:04 +0330

    Update and rename 09 seconde.py to 09 convert temperature.py

---
+++ challenges/1 Basic Concepts/09 convert temperature.py
@@ -0,0 +1,3 @@
+# Write a program that converts a temperature in Fahrenheit to Celsius. The temperature should be stored as a variable and printed out in both Fahrenheit and Celsius.
+fahrenheit = float(input("Enter the temperature in Fahrenheit: "))
+celsius = (fahrenheit - 32) * 5 / 9
