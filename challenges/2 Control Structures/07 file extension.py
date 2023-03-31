27c2618c21f4d64e6caec4d3edde1739e4fab595
Mohammad Shojaei <shojaeireal@gmail.com>
2023-03-31 12:07:13 +0330

    Update 07 file extension.py

--- challenges/2 Control Structures/07 file extension.py
+++ challenges/2 Control Structures/07 file extension.py
@@ -1,3 +1,17 @@
+'''
+Write a function that takes a filename as input, removes any spaces from the filename and converts it to all lowercase characters.
+The function should then check the extension of the file and print the MIME type of the file according to the following rules:
+
+If the file has a ".gif" extension, print "image/gif"
+If the file has a ".jpg" extension, print "image/jpg"
+If the file has a ".jpeg" extension, print "image/jpeg"
+If the file has a ".png" extension, print "image/png"
+If the file has a ".pdf" extension, print "application/pdf"
+If the file has a ".zip" extension, print "application/zip"
+If the file has a ".txt" extension, print "text/plain"
+Otherwise, print "application/octet-stream"
+'''
+
 # Get user input
 filename = input("Name of the file: ")
 # Remove spaces and make it all lower
