e80e186e0beab3bfd45360473f97f60753e0968c
Mohammad Shojaei <shojaeireal@gmail.com>
2023-03-31 21:12:28 +0330

    Update 08 longest word.py

--- challenges/2 Control Structures/08 longest word.py
+++ challenges/2 Control Structures/08 longest word.py
@@ -1,13 +1,5 @@
-# Write a program that prompts the user to enter a sentence and then uses a while loop to print out the longest word in the sentence.
+# Write a program that prints out all possible combinations of two-digit numbers from 00 to 99, where each digit in the combination is odd.
 
-sentence = input("Enter a sentence: ")
-words = sentence.split()
-longest_word = ""
-
-i = 0
-while i < len(words):
-    if len(words[i]) > len(longest_word):
-        longest_word = words[i]
-    i += 1
-
-print("The longest word in the sentence is:", longest_word)
+for i in range(1, 10, 2):
+    for j in range(1, 10, 2):
+        print(str(i) + str(j))
