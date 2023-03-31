1fdebb784dfe0e3728ba1fdbde04e4699346e654
Mohammad Shojaei <shojaeireal@gmail.com>
2023-03-31 11:58:37 +0330

    Delete 03 umbrella.py

--- challenges/2 Control Structures/03 umbrella.py
+++
@@ -1,8 +0,0 @@
-status = input("is it raining? yes/no :")
-status = status.lower()
-if status == "yes":
-    print("you should take an umbrella")
-elif status == "no":
-    print("you you don't need an umbrella")
-else:
-    print("please just enter 'yes or 'no'  ")
