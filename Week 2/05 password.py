eaa0ac3cc40a685942e77226c852f615ce64131f
Mohammad Shojaei <shojaeireal@gmail.com>
2023-04-27 23:29:23 +0330

    Delete Week 2 directory

--- Week 2/05 password.py
+++
@@ -1,9 +0,0 @@
-# Write a program that prompts the user to enter a password. If the password is "password123", print a message indicating that the password has been accepted. Otherwise, 
-# print a message indicating that the password is incorrect.
-
-password = input("Enter your password: ")
-
-if password == "password123":
-    print("Password accepted!")
-else:
-    print("Incorrect password.")
